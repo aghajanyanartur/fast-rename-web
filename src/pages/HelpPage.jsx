a571d435ec6b325f1da3abf64a0aa35573e10174
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-12-18 13:12:15 -0400

    Help instructions updated

--- src/pages/HelpPage.jsx
+++ src/pages/HelpPage.jsx
@@ -9,44 +9,53 @@ const HelpPage = () => {
             <div style={{ paddingBottom: '10vh', paddingTop: `${isPortrait ? '' : '5vh'}` }}>
                 <h1 className={`${isPortrait ? 'text-[5vw]' : 'text-[3vw]'} text-blue-950`}>How to Use Fast Rename</h1>
 
-                <p className={`${isPortrait ? 'text-[3vw] w-[80vw] pt-2 pb-1' : 'text-[1.5vw] w-[60vw] pt-8 pb-2'}  text-left`}>1. Copy and paste the folder path</p>
+                <p className={`${isPortrait ? 'text-[3vw] w-[80vw] pt-2 pb-1' : 'text-[1.5vw] w-[60vw] pt-8 pb-2'}  text-left`}>Choose the folder</p>
                 <p className={`${isPortrait ? 'text-[2vw] w-[80vw] p-3 rounded-2xl' : 'text-[1vw] w-[60vw] p-5 rounded-3xl'} text-left  bg-gray-300`}>
-                    Enter the path of the folder you want to access or use. This involves copying the exact location of the folder from your computer's file system and pasting it into the designated field. For example, on a Windows system, it might look like "C:\Users\YourName\Documents\MyFolder". This address tells the program where to find or store files within the specific folder you've identified.
+                    Copy and paste the folder path, or just browse and open it.
                 </p>
 
-                <p className={`${isPortrait ? 'text-[3vw] w-[80vw] pt-4 pb-1' : 'text-[1.5vw] w-[60vw] pt-8 pb-2'}  text-left`}>2. Select the starting number</p>
+                <p className={`${isPortrait ? 'text-[3vw] w-[80vw] pt-4 pb-1' : 'text-[1.5vw] w-[60vw] pt-8 pb-2'}  text-left`}>Filter and sort</p>
                 <p className={`${isPortrait ? 'text-[2vw] w-[80vw] p-3 rounded-2xl' : 'text-[1vw] w-[60vw] p-5 rounded-3xl'} text-left  bg-gray-300`}>
-                    Choose a specific number to begin the sequence. For instance, if you select '1', the sequence will start from 1 and continue as 1, 2, 3, and so on. This initial number sets the base for the subsequent numbers in the series.
+                    <i>Filter by extension:</i> You can filter the files by extension if needed. Extension should be specified without a dot. For example, if you want to rename all the files with the extension '.txt', you should type 'txt' in the filter field<br /><br />
+                    <i>Sort alphabetically:</i> If you choose this option, the files will be sorted alphabetically <i>before</i> renaming.
                 </p>
 
-                <p className={`${isPortrait ? 'text-[3vw] w-[80vw] pt-4 pb-1' : 'text-[1.5vw] w-[60vw] pt-8 pb-2'}  text-left`}>3. Decide the number of digits to fill</p>
+                <p className={`${isPortrait ? 'text-[3vw] w-[80vw] pt-4 pb-1' : 'text-[1.5vw] w-[60vw] pt-8 pb-2'}  text-left`}>Use current name</p>
                 <p className={`${isPortrait ? 'text-[2vw] w-[80vw] p-3 rounded-2xl' : 'text-[1vw] w-[60vw] p-5 rounded-3xl'} text-left  bg-gray-300`}>
-                    Specify the total length for a number by setting a digit count. If the original number has fewer digits than this count, it will be padded with leading zeros until it reaches the set length. For example, if you choose '4' as the number of digits to fill and your number is '5', it will be formatted as '0005'. This setting is often used to standardize the format of numerical data, especially in cases where uniform digit counts are required.
+                    <i>Cases:</i> You can keep the cases in the names of the files as they are. otherwise, you can make the names of the files uppercase or lowercase. This applies only to the <i>current names</i> of the files.<br /><br />
+                    <i>Replace:</i> Find and replace a specific text in the current names of the files. For example, if you want to replace all the occurrences of the word 'file' with 'document', you should type 'file' in the 'Find' field and 'document' in the 'Replace' field.
                 </p>
 
-                <p className={`${isPortrait ? 'text-[3vw] w-[80vw] pt-4 pb-1' : 'text-[1.5vw] w-[60vw] pt-8 pb-2'}  text-left`}>4. Choose incrementation step</p>
+                <p className={`${isPortrait ? 'text-[3vw] w-[80vw] pt-4 pb-1' : 'text-[1.5vw] w-[60vw] pt-8 pb-2'}  text-left`}>Define counter</p>
                 <p className={`${isPortrait ? 'text-[2vw] w-[80vw] p-3 rounded-2xl' : 'text-[1vw] w-[60vw] p-5 rounded-3xl'} text-left  bg-gray-300`}>
-                    Choose the value by which the sequence of numbers will increase. For example, if you select '2', each number in the sequence will be 2 greater than the previous one (e.g., 1, 3, 5, 7). This setting determines the step size between consecutive numbers in your sequence, allowing for regular or custom intervals.
+                    <i>Starting number:</i> The number from which the counter starts. For example, if you set the start number to 10, the first file will be named 10, the second 11, and so on.<br /><br />
+                    <i>Number of digits:</i> The number of digits in the counter. For example, if you set the number of digits to 3, the first file will be named 001, the second 002, and so on.<br /><br />
+                    <i>Step size:</i> The step size of the counter. For example, if you set the step size to 2, the first file will be named 10, the second 12, the third 14, and so on.<br /><br />
+                    <i>Decrement:</i> Selecting this checkbox reverses the increment behavior. Instead of numbers increasing, they will decrease by the specified step size. For example, if your starting number is 10 and the increment is set to 2, enabling 'Decrement' will change the sequence to decrease, resulting in 10, 8, 6, etc.
                 </p>
 
-                <p className={`${isPortrait ? 'text-[3vw] w-[80vw] pt-4 pb-1' : 'text-[1.5vw] w-[60vw] pt-8 pb-2'}  text-left`}>5. Add a name pattern</p>
+                <p className={`${isPortrait ? 'text-[3vw] w-[80vw] pt-4 pb-1' : 'text-[1.5vw] w-[60vw] pt-8 pb-2'}  text-left`}>Add date</p>
                 <p className={`${isPortrait ? 'text-[2vw] w-[80vw] p-3 rounded-2xl' : 'text-[1vw] w-[60vw] p-5 rounded-3xl'} text-left  bg-gray-300`}>
-                    Specify (additionally) the format or structure for naming items, such as files or records. This pattern can include a combination of fixed text, numbers, and special characters. For instance, if you set a name pattern as "Document_#", each item will be named starting with "Document_" followed by a unique number or identifier (e.g., Document_1, Document_2). This feature is useful for organizing and identifying items systematically within a database, a file system, or any collection requiring consistent naming conventions.
+                    <i>Select the date:</i> Choose whether to append a date or not. If yes, you can choose from the current date, the date the item was last modified, or the date it was created.<br/><br/>
+                    <i>Format of date:</i> Choose the format of the date from the list. You can choose the sequence of day, month, and year, and the separators between them. For example, you could format the date as "yyyy-mm-dd" (e.g., 2023-12-15) or "dd.mm.yyyy" (e.g., 15.12.2023).
                 </p>
 
-                <p className={`${isPortrait ? 'text-[3vw] w-[80vw] pt-4 pb-1' : 'text-[1.5vw] w-[60vw] pt-8 pb-2'}  text-left`}>6. Add Date If Needed</p>
+                <p className={`${isPortrait ? 'text-[3vw] w-[80vw] pt-4 pb-1' : 'text-[1.5vw] w-[60vw] pt-8 pb-2'}  text-left`}>Create name pattern</p>
                 <p className={`${isPortrait ? 'text-[2vw] w-[80vw] p-3 rounded-2xl' : 'text-[1vw] w-[60vw] p-5 rounded-3xl'} text-left  bg-gray-300`}>
-                    This feature allows you to append a date to your naming pattern, with options to choose the type of date and its format. You can select from the current date, the date the item was last modified, or the date it was created. Additionally, you have the flexibility to customize the date format, including the sequence of day, month, and year, and the separators between them. For example, you could format the date as "YYYY-MM-DD" (e.g., 2023-12-15) or "DD/MM/YYYY" (e.g., 15/12/2023), depending on your preference. This functionality is useful for organizing items chronologically and maintaining a clear record of timelines.
+                    <i>Add any text:</i> You can add any text to the name pattern. Just write document in the pattern line.<br/><br/>
+                    <i>Add components:</i> Use button to add current name, counter and/or date. All modifications you made in the previous steps are already applied to the components. You can add more than one from each component and in any order. Consider using counter, to avoid name conflicts.<br/><br/>
+                    <i>Examlpe:</i> Given the pattern "document_{'{date}'}-No.{'{counter}'}", (assuming you have chosen the current date in "mm-dd-yyyy" format and a counter starting from 1, with a step size of 1, and number of digits - 3), the file names will go "document_12-15-2023_No.001", "document_12-15-2023_No.002", "document_12-15-2023_No.003", and so on
                 </p>
 
-                <p className={`${isPortrait ? 'text-[3vw] w-[80vw] pt-4 pb-1' : 'text-[1.5vw] w-[60vw] pt-8 pb-2'}  text-left`}>7. Choose additional options</p>
+                <p className={`${isPortrait ? 'text-[3vw] w-[80vw] pt-4 pb-1' : 'text-[1.5vw] w-[60vw] pt-8 pb-2'}  text-left`}>Change extension</p>
                 <p className={`${isPortrait ? 'text-[2vw] w-[80vw] p-3 rounded-2xl' : 'text-[1vw] w-[60vw] p-5 rounded-3xl'} text-left  bg-gray-300`}>
-                    <i>Decrement:</i> This checkbox reverses the increment behavior. Instead of numbers increasing, they will decrease by the specified step size. For example, if your starting number is 10 and the increment is set to 2, enabling 'Decrement' will change the sequence to decrease, resulting in 10, 8, 6, etc. This is useful when you need a descending order in your numbering.<br /><br />
-                    <i>Name Pattern Trailing:</i> When this option is selected, the specified name pattern will be placed after the numbers or dates in the naming convention. For example, if your number sequence is "1, 2, 3" and your name pattern is "_Document", with this option enabled, the resulting names would be "1_Document", "2_Document", "3_Document", etc. This feature essentially reverses the usual order, putting the descriptive part of the name (like 'Document') after the dynamic numerical or date component, which can be useful for certain organizational or sorting purposes.<br /><br />
-                    <i>Sort Alphabetically Before Renaming:</i> This option sorts the items alphabetically based on their current names before applying the new naming convention. It's particularly useful when you want to maintain an alphabetical order in the new names. For example, if you are renaming a list of files, they will first be sorted by their original names and then renamed according to your specified pattern and sequence. This helps in keeping files organized and easily searchable.<br /><br />
+                    You have an option to change the extension of the files. All renamed files will have newly specified extension. Be cautious, as this may cause some files to become unusable. Extension should be specified without a dot.
                 </p>
 
-                <p className={`${isPortrait ? 'text-[3vw] w-[80vw] pt-4 pb-1' : 'text-[1.5vw] w-[60vw] pt-8 pb-2'}  text-left`}>8. Click the 'Rename' button.</p>
+                <p className={`${isPortrait ? 'text-[3vw] w-[80vw] pt-4 pb-1' : 'text-[1.5vw] w-[60vw] pt-8 pb-2'}  text-left`}>Save configurations</p>
+                <p className={`${isPortrait ? 'text-[2vw] w-[80vw] p-3 rounded-2xl' : 'text-[1vw] w-[60vw] p-5 rounded-3xl'} text-left  bg-gray-300`}>
+                    Save your settings to a file. Use 'Save configuration file' option in 'File' menu. Saving the configuration file may be useful, if you will need to rename files later with the same setting.
+                </p>
             </div>
         </div>
     );
